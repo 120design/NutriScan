55f199127e4ea0c3007d38738c1d1caf9200729a
VIncent Caronnet <120@120.design>
2020-12-21 14:07:43 +0100

    Initial Commit

---
+++ NutriScan/NutriScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriScanApp.swift
+//  NutriScan
+//
+//  Created by Vincent Caronnet on 21/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct NutriScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
