a9d80c352f1db569fdaee7d2ab74d57cfa381e21
Vincent Caronnet <120@120.design>
2021-10-26 12:23:55 +0200

    Ajout de l’AppIcon de la version Pro Les paragraphes sous le formulaire de l’EANView ne sont plus masqué quand le clavier est visible et ils ne sont pas réduits non plus

--- NutriScan/View/Search/EANView.swift
+++ NutriScan/View/Search/EANView.swift
@@ -10,100 +10,94 @@ import SwiftUI
 struct EANView: View {
     @EnvironmentObject var searchViewModel: SearchViewModel
     
-    @FocusState private var isTextFieldFocused: Bool
-    
     var body: some View {
         VStack {
-            TextField("", text: $searchViewModel.eanCode)
-                .padding(14)
-                .font(.system(size: 16))
-                .foregroundColor(.nuSecondaryColor)
-                .accentColor(.nuSecondaryColor)
-                .modifier(
-                    NUPlaceholderStyleModifier(
-                        showPlaceHolder: searchViewModel.eanCode.isEmpty,
-                        placeholder: "Exemple : 8712100325953",
-                        foregroundColor: .nuSecondaryColor
-                    )
-                )
-                .keyboardType(.numberPad)
-                .focused($isTextFieldFocused)
-                .overlay(
-                    RoundedRectangle(
-                        cornerRadius: 12,
-                        style: .continuous
-                    )
-                    .stroke(Color.nuSecondaryColor, lineWidth: 1)
-                )
-                .background(
-                    Color.nuSecondaryColor.opacity(0.2)
-                        .mask(
-                            RoundedRectangle(
-                                cornerRadius: 12,
-                                style: .continuous
-                            )
+            VStack {
+                TextField("", text: $searchViewModel.eanCode)
+                    .padding(14)
+                    .font(.system(size: 16))
+                    .foregroundColor(.nuSecondaryColor)
+                    .accentColor(.nuSecondaryColor)
+                    .modifier(
+                        NUPlaceholderStyleModifier(
+                            showPlaceHolder: searchViewModel.eanCode.isEmpty,
+                            placeholder: "Exemple : 8712100325953",
+                            foregroundColor: .nuSecondaryColor
                         )
-                )
-                .padding(.bottom, 5)
-//                .animation(nil)
-            HStack {
-                Button(action: {
-                    searchViewModel.eanCode = ""
-                }, label: {
-                    Text("Effacer")
-                        .padding(.vertical, 10)
-                        .padding(.horizontal, 6)
-                        .frame(maxWidth: .infinity)
-                        .background(Color.nuSecondaryColor)
-                        .modifier(NUSmoothCornersModifier(cornerRadius: 12))
-                })
-                .disabled(searchViewModel.clearButtonIsDisabled)
-                .opacity(searchViewModel.clearButtonIsDisabled ? 0.6 : 1)
-                
-                Button(action: {
-                    searchViewModel.getProduct()
-                }, label: {
-                    Text("Rechercher")
-                        .padding(.vertical, 10)
-                        .padding(.horizontal, 6)
-                        .frame(maxWidth: .infinity)
-                        .background(Color.nuTertiaryColor)
-                        .modifier(NUSmoothCornersModifier(cornerRadius: 12))
-                })
-                .disabled(searchViewModel.eanCode.isEmpty)
-                .opacity(searchViewModel.searchButtonIsDisabled ? 0.6 : 1)
+                    )
+                    .keyboardType(.numberPad)
+                    .overlay(
+                        RoundedRectangle(
+                            cornerRadius: 12,
+                            style: .continuous
+                        )
+                            .stroke(Color.nuSecondaryColor, lineWidth: 1)
+                    )
+                    .background(
+                        Color.nuSecondaryColor.opacity(0.2)
+                            .mask(
+                                RoundedRectangle(
+                                    cornerRadius: 12,
+                                    style: .continuous
+                                )
+                            )
+                    )
+                    .padding(.bottom, 5)
+                //                .animation(nil)
+                HStack {
+                    Button(action: {
+                        searchViewModel.eanCode = ""
+                    }, label: {
+                        Text("Effacer")
+                            .padding(.vertical, 10)
+                            .padding(.horizontal, 6)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.nuSecondaryColor)
+                            .modifier(NUSmoothCornersModifier(cornerRadius: 12))
+                    })
+                        .disabled(searchViewModel.clearButtonIsDisabled)
+                        .opacity(searchViewModel.clearButtonIsDisabled ? 0.6 : 1)
+                    
+                    Button(action: {
+                        searchViewModel.getProduct()
+                    }, label: {
+                        Text("Rechercher")
+                            .padding(.vertical, 10)
+                            .padding(.horizontal, 6)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.nuTertiaryColor)
+                            .modifier(NUSmoothCornersModifier(cornerRadius: 12))
+                    })
+                        .disabled(searchViewModel.eanCode.isEmpty)
+                        .opacity(searchViewModel.searchButtonIsDisabled ? 0.6 : 1)
+                }
+                .modifier(NUButtonLabelModifier())
             }
-            .modifier(NUButtonLabelModifier())
-        }
-        .padding()
-        .background(
-            Color
-                .nuPrimaryColor
-                .modifier(NUSmoothCornersModifier(cornerRadius: 28))
-        )
-        .padding()
-        
-        if !isTextFieldFocused {
-            VStack(alignment: .leading) {
-                Text("Recherchez un produit après avoir soit renseigné *un code EAN13 à treize chiffres,* soit renseigné *un code EAN8 à huit chiffres.*")
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .font(nuBodyBookTextFont)
-            .foregroundColor(.nuPrimaryColor)
-            .padding([.leading, .trailing])
-            .padding(.bottom, 8)
+            .padding()
+            .background(
+                Color
+                    .nuPrimaryColor
+                    .modifier(NUSmoothCornersModifier(cornerRadius: 28))
+            )
+            .padding()
             
-            VStack(alignment: .leading) {
-                Text("Ce code est généralement inscrit *sous le code à barres* du produit. Une fois le code tapé, *pressez le bouton de recherche.*")
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .font(nuBodyBookTextFont)
-            .foregroundColor(.nuPrimaryColor)
-            .padding([.leading, .trailing])
-            .padding(.bottom, 8)
+            Text("Recherchez un produit après avoir soit renseigné *un code EAN13 à treize chiffres,* soit renseigné *un code EAN8 à huit chiffres.*")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(nuBodyBookTextFont)
+                .foregroundColor(.nuPrimaryColor)
+                .padding([.leading, .trailing])
+                .padding(.bottom, 8)
+            
+            Text("Ce code est généralement inscrit *sous le code à barres* du produit. Une fois le code tapé, *pressez le bouton de recherche.*")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(nuBodyBookTextFont)
+                .foregroundColor(.nuPrimaryColor)
+                .padding([.leading, .trailing])
+                .padding(.bottom, 8)
+            
+            Spacer()
         }
-
-        Spacer()
+        .ignoresSafeArea(.keyboard)
     }
 }
 
